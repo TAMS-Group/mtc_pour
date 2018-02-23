0a3774a8472bf5aca93463abb0380bd41327e554
v4hn <me@v4hn.de>
2018-02-23 16:44:41 +0100

    initial commit: first successful mtc with pouring
    It does not stop pouring though...

---
+++ src/pour_into.cpp
@@ -0,0 +1,221 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2017-2018, Hamburg University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Bielefeld University nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Authors: Michael Goerner, Henning Kayser
+   Desc:    Pour from attached bottle into(onto) an object
+*/
+
+#include "pour_into.h"
+
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit/trajectory_processing/iterative_time_parameterization.h>
+
+#include <shape_msgs/SolidPrimitive.h>
+
+#include <eigen_conversions/eigen_msg.h>
+
+#include <rviz_marker_tools/marker_creation.h>
+
+namespace mtc_pour {
+
+PourInto::PourInto(std::string name) :
+	PropagatingForward(std::move(name))
+{
+	auto& p = properties();
+	p.declare<std::string>("group", "name of planning group");
+
+	p.declare<std::string>("bottle", "attached bottle-like object");
+	p.declare<std::string>("container", "container object to be filled");
+
+	p.declare<double>("tilt_angle", "maximum tilt-angle for the bottle");
+	p.declare<Eigen::Vector3d>("pour_offset", "offset for the bottle tip w.r.t. container top-center during pouring");
+}
+
+void PourInto::init(const planning_scene::PlanningSceneConstPtr& scene) {
+	PropagatingForward::init(scene);
+}
+
+bool PourInto::computeForward(const InterfaceState& from) {
+	planning_scene::PlanningScenePtr to;
+	SubTrajectory trajectory;
+
+	bool success = !compute(from, to, trajectory);
+	sendForward(from, InterfaceState(to), std::move(trajectory));
+	return success;
+}
+
+namespace {
+/** Compute prototype waypoints for pouring.
+  * This generates a trajectory that pours in the Y-Z axis.
+  * The generated poses are for the bottle-tip and are relative to the container top-center */
+void computePouringWaypoints(const Eigen::Affine3d& start_tip_pose, double tilt_angle,
+                             const Eigen::Translation3d& pouring_offset, EigenSTL::vector_Affine3d& waypoints,
+                             unsigned int nr_of_waypoints= 10) {
+	Eigen::Affine3d tip_pose(start_tip_pose);
+
+	waypoints.push_back(start_tip_pose);
+
+	for(unsigned int i= 1; i <= nr_of_waypoints; ++i){
+		const double fraction= (double) i / nr_of_waypoints;
+		const double exp_fraction= fraction*fraction;
+
+		// linear interpolation for tilt angle
+		Eigen::AngleAxisd rotation(fraction*tilt_angle, Eigen::Vector3d::UnitX());
+
+		// exponential interpolation towards container rim + offset
+		Eigen::Translation3d translation(
+			start_tip_pose.translation()     * (1-exp_fraction) +
+			pouring_offset.translation() * exp_fraction
+			);
+
+		waypoints.push_back(translation*rotation);
+	}
+}
+}
+
+bool PourInto::compute(const InterfaceState& input, planning_scene::PlanningScenePtr& result, SubTrajectory& trajectory) {
+	const auto& props= properties();
+
+	const std::string& container_name= props.get<std::string>("container");
+	const std::string& bottle_name= props.get<std::string>("bottle");
+
+	const Eigen::Translation3d pour_offset( props.get<Eigen::Vector3d>("pour_offset") );
+	const auto& tilt_angle= props.get<double>("tilt_angle");
+
+	const planning_scene::PlanningScene& scene= *input.scene();
+	moveit::core::RobotModelConstPtr robot_model= scene.getRobotModel();
+	const moveit::core::JointModelGroup* group= robot_model->getJointModelGroup(props.get<std::string>("group"));
+
+	moveit_msgs::CollisionObject container;
+	if(!scene.getCollisionObjectMsg(container, container_name)){
+		ROS_ERROR_STREAM("container object '" << container_name << "' is not specified in input planning scene");
+		return false;
+	}
+	if(container.primitives[0].type != shape_msgs::SolidPrimitive::CYLINDER){
+		ROS_ERROR("PourInto expects container to be a cylinder");
+		return false;
+	}
+
+	moveit_msgs::AttachedCollisionObject bottle;
+	if(!scene.getAttachedCollisionObjectMsg(bottle, bottle_name)){
+		ROS_ERROR_STREAM("bottle '" << bottle_name << "' is not an attached collision object in input planning scene");
+		return false;
+	}
+	if(bottle.object.primitives[0].type != shape_msgs::SolidPrimitive::CYLINDER){
+		ROS_ERROR("PourInto expects bottle object to have a cylinder as bottle tip as first primitive");
+		return false;
+	}
+
+	moveit::core::RobotState state(scene.getCurrentState());
+
+	// container frame: top-center of container object
+   const Eigen::Affine3d& container_frame=
+		scene.getFrameTransform(container_name) *
+		Eigen::Translation3d(Eigen::Vector3d(0,0,container.primitives[0].dimensions[0]/2));
+
+	const Eigen::Affine3d& bottle_frame= scene.getFrameTransform(bottle_name);
+
+	// assume bottle tip as top-center of cylinder
+	const Eigen::Translation3d bottle_tip(Eigen::Vector3d(0, 0, bottle.object.primitives[0].dimensions[0]/2));
+
+	auto& attached_bottle_tfs= state.getAttachedBody(bottle_name)->getFixedTransforms();
+
+	assert(attached_bottle_tfs.size() > 0 && "impossible: attached body does not know transform to its link");
+	const Eigen::Affine3d bottle_tip_in_tool_link(attached_bottle_tfs[0]*bottle_tip);
+
+	const Eigen::Affine3d bottle_tip_in_container_frame=
+		container_frame.inverse() *
+		bottle_frame *
+		bottle_tip;
+
+	EigenSTL::vector_Affine3d waypoints;
+	computePouringWaypoints(bottle_tip_in_container_frame, tilt_angle, pour_offset, waypoints);
+
+	for(auto& waypoint : waypoints)
+		waypoint= container_frame*waypoint;
+
+	for(auto waypoint : waypoints){
+		geometry_msgs::PoseStamped p;
+		p.header.frame_id= scene.getPlanningFrame();
+		tf::poseEigenToMsg(waypoint, p.pose);
+		// TODO: rename or move this package. could be moved to moveit_visual_tools
+		rviz_marker_tools::appendFrame(trajectory.markers(), p, 0.1, "pouring waypoints");
+	}
+
+	for(auto& waypoint : waypoints)
+		waypoint= waypoint*bottle_tip_in_tool_link.inverse();
+
+	std::vector<moveit::core::RobotStatePtr> traj; // TODO: multi-waypoint callback in cartesian_planner?
+
+	double path_fraction= state.computeCartesianPath(
+		group,
+		traj,
+		state.getLinkModel(bottle.link_name),
+		waypoints,
+		true /* global reference_frame */,
+		.02 /* max step size */,
+		2.0 /* jump threshold */,
+		[&scene](moveit::core::RobotState* rs,
+		         const moveit::core::JointModelGroup* jmg,
+		         const double* joint_positions){
+			rs->setJointGroupPositions(jmg, joint_positions);
+			rs->update();
+			return !scene.isStateColliding(*rs, jmg->getName());
+			}
+		);
+
+	auto robot_trajectory = std::make_shared<robot_trajectory::RobotTrajectory>(robot_model, group);
+	for(const auto& waypoint : traj){
+		robot_trajectory->addSuffixWayPoint(waypoint, 0.0);
+	}
+
+	trajectory_processing::IterativeParabolicTimeParameterization iptp;
+	iptp.computeTimeStamps(*robot_trajectory);
+
+	trajectory.setTrajectory(robot_trajectory);
+
+	result= scene.diff();
+	result->setCurrentState(*traj.back());
+
+	if ( path_fraction < 1.0 - .1 ){
+		ROS_WARN_STREAM("PourInto only produced motion for " << path_fraction << " of the way. Rendering invalid");
+		trajectory.setCost(std::numeric_limits<double>::infinity());
+		return false;
+	}
+
+	return true;
+}
+
+}
---
+++ src/tams_ur5_pouring_mtc_demo.cpp
@@ -0,0 +1,181 @@
+#include <ros/ros.h>
+
+#include <moveit/robot_model/robot_model.h> // TODO shouldn't be necessary...
+
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+
+#include <moveit/task_constructor/task.h>
+
+#include <moveit/task_constructor/stages/current_state.h>
+#include <moveit/task_constructor/stages/move_to.h>
+#include <moveit/task_constructor/stages/move_relative.h>
+#include <moveit/task_constructor/stages/connect.h>
+#include <moveit/task_constructor/stages/compute_ik.h>
+
+#include "pour_into.h"
+
+#include <moveit/task_constructor/stages/modify_planning_scene.h>
+
+#include <moveit/task_constructor/stages/generate_grasp_pose.h>
+
+#include <moveit/task_constructor/solvers/cartesian_path.h>
+#include <moveit/task_constructor/solvers/pipeline_planner.h>
+
+void spawnObjects(){
+	moveit::planning_interface::PlanningSceneInterface psi;
+
+	std::vector<moveit_msgs::CollisionObject> objects;
+
+	moveit_msgs::CollisionObject o;
+	o.id= "bottle";
+	o.header.frame_id= "table_top";
+	o.primitive_poses.resize(1);
+	o.primitive_poses[0].position.x= -0.15;
+	o.primitive_poses[0].position.y= 0.25;
+	o.primitive_poses[0].position.z= 0.12;
+	o.primitive_poses[0].orientation.w= 1.0;
+	o.primitives.resize(1);
+	o.primitives[0].type= shape_msgs::SolidPrimitive::CYLINDER;
+	o.primitives[0].dimensions.resize(2);
+	o.primitives[0].dimensions[0]= 0.23;
+	o.primitives[0].dimensions[1]= 0.03;
+	objects.push_back(o);
+
+	o.id= "glass";
+	o.primitive_poses[0].position.x= -0.2;
+	o.primitive_poses[0].position.y= -0.05;
+	o.primitive_poses[0].position.z= 0.06;
+	o.primitives[0].dimensions[0]= 0.11;
+	objects.push_back(o);
+
+	psi.applyCollisionObjects(objects);
+}
+
+using namespace moveit::task_constructor;
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "mtc_pouring");
+
+	ros::AsyncSpinner spinner(1);
+	spinner.start();
+
+	spawnObjects();
+
+	// TODO: why does a restart trigger a new panel entry
+	Task t;
+
+   // TODO: id of solution in rviz panel is sometimes 0 and then changes
+
+	auto sampling_planner = std::make_shared<solvers::PipelinePlanner>();
+	// TODO: timeout is still 10 in move to pre-grasp
+	sampling_planner->setTimeout(15.0);
+	//pipeline->setPlannerId("");
+
+	auto cartesian_planner = std::make_shared<solvers::CartesianPath>();
+
+	t.setProperty("group", "arm");
+	t.setProperty("eef", "gripper");
+	t.setProperty("gripper", "gripper"); // TODO: use this
+
+	t.add(std::make_unique<stages::CurrentState>("current state"));
+
+	{
+		auto stage = std::make_unique<stages::MoveTo>("open gripper", sampling_planner);
+		stage->setGroup("gripper");
+		stage->setGoal("open");
+		t.add(std::move(stage));
+	}
+
+	{
+		auto stage = std::make_unique<stages::Connect>("move to pre-grasp pose", sampling_planner);
+		stage->properties().configureInitFrom(Stage::PARENT); // TODO: convenience-wrapper
+		t.add(std::move(stage));
+	}
+
+	{
+		auto stage = std::make_unique<stages::MoveRelative>("approach object", cartesian_planner);
+		stage->properties().set("marker_ns", "approach"); // TODO: convenience wrapper
+		stage->properties().set("link", "s_model_tool0");
+		stage->properties().configureInitFrom(Stage::PARENT, {"group"});
+		stage->setMinMaxDistance(.08, .15);
+
+		geometry_msgs::Vector3Stamped vec;
+		vec.header.frame_id = "s_model_tool0";
+		vec.vector.x = 1.0;
+		stage->along(vec);
+		t.add(std::move(stage));
+	}
+
+	{
+		auto stage = std::make_unique<stages::GenerateGraspPose>("grasp work space pose");
+		stage->properties().configureInitFrom(Stage::PARENT);
+		stage->setGripperGraspPose("open");
+		stage->setObject("bottle");
+		stage->setToolToGraspTF(Eigen::Translation3d(0,0,0), "s_model_tool0");
+		stage->setAngleDelta(M_PI/6);
+
+		auto wrapper = std::make_unique<stages::ComputeIK>("grasp pose", std::move(stage) );
+		wrapper->setMaxIKSolutions(8);
+		// TODO adding this will initialize "target_pose" which is internal (or isn't it?)
+		//wrapper->properties().configureInitFrom(Stage::PARENT);
+		wrapper->properties().configureInitFrom(Stage::PARENT, {"eef"}); // TODO: convenience wrapper
+		t.add(std::move(wrapper));
+	}
+
+	{
+		auto stage = std::make_unique<stages::ModifyPlanningScene>("allow gripper->object collision");
+		stage->enableCollisions("bottle", t.getRobotModel()->getJointModelGroup("gripper")->getLinkModelNamesWithCollisionGeometry(), true);
+		t.add(std::move(stage));
+	}
+
+	{
+		auto stage = std::make_unique<stages::MoveTo>("close gripper", sampling_planner);
+		stage->properties().property("group").configureInitFrom(Stage::PARENT, "gripper"); // TODO this is not convenient
+		stage->setGoal("closed");
+		t.add(std::move(stage));
+	}
+
+	{
+		auto stage = std::make_unique<stages::ModifyPlanningScene>("attach object");
+		stage->attachObject("bottle", "s_model_tool0");
+		t.add(std::move(stage));
+	}
+
+	{
+		auto stage = std::make_unique<stages::MoveRelative>("lift object", cartesian_planner);
+		stage->properties().configureInitFrom(Stage::PARENT, {"group"});
+		stage->setMinMaxDistance(.25,.3);
+		stage->setLink("s_model_tool0"); // TODO property for frame
+
+		stage->properties().set("marker_ns", "lift");
+
+		geometry_msgs::Vector3Stamped vec;
+		vec.header.frame_id= "table_top";
+		vec.vector.z= 1.0;
+		stage->along(vec);
+		t.add(std::move(stage));
+	}
+
+	{
+		auto stage = std::make_unique<mtc_pour::PourInto>("test pouring");
+		stage->setBottle("bottle");
+		stage->setContainer("glass");
+		stage->setPourOffset(Eigen::Vector3d(0,0.01,0.03));
+		stage->setTiltAngle(2.0);
+		stage->properties().configureInitFrom(Stage::PARENT);
+		t.add(std::move(stage));
+	}
+
+	t.enableIntrospection();
+
+	ROS_INFO_STREAM( t );
+
+	// TODO: t.validate();
+
+	t.plan();
+
+	std::cout << "waiting for <enter>" << std::endl;
+	std::cin.get();
+
+	return 0;
+}
