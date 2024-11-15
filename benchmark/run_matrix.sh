114c2df90cc7bb62fe43d7c81c8641cf4b579260
v4hn <me@v4hn.de>
2024-11-15 20:39:50 +0100

    more jobs & matrix entries
    for server testing

--- benchmark/run_matrix.sh
+++ benchmark/run_matrix.sh
@@ -5,9 +5,9 @@ OUT=${1:-out}
 mkdir -p $OUT
 
 for divider in false true; do
-parallel --bar --eta -j1 --shuf --header : \
-   "rosrun mtc_pour mtc_pour_tams_ur5_demo _with_path_constraint:=true _spawn_objects:=true _divider:=$divider _keep_running:=false _introspection:=false _workers:={workers} _solutions:={solutions} _connect_compute_attempts:={workers} > $OUT/pour_{workers}workers_{solutions}solutions_rep{repetition}_divider:$divider.log 2>&1" \
-   ::: workers $(seq -1 7) \
+parallel --bar --eta -j3 --shuf --header : \
+   "rosrun mtc_pour mtc_pour_tams_ur5_demo _with_path_constraint:=true _spawn_objects:=true _divider:=$divider _keep_running:=false _introspection:=false __name:=mtc_{#} _workers:={workers} _solutions:={solutions} _connect_compute_attempts:={workers} > $OUT/pour_{workers}workers_{solutions}solutions_rep{repetition}_divider:$divider.log 2>&1" \
+   ::: workers -1 $(seq 1 15) \
    ::: repetition $(seq 10) \
-   ::: solutions 1 10 100 \
+   ::: solutions 1 10 100
 done
